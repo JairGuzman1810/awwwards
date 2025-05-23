7c44b8a6baa403684c605fe5c2a1a254270d85a0
JairGuzman1810 <jair18101@hotmail.com>
2025-05-23 12:19:37 -0600

    feat: add foreground headlines to Hero component for enhanced visual appeal

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -79,6 +79,20 @@ const Hero = () => {
             onLoadedData={handleVideoLoad}
           />
         </div>
+
+        {/* Foreground "Gaming" headline at bottom right */}
+        <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+          G<b>a</b>ming
+        </h1>
+
+        {/* Foreground "redefine" headline at top left */}
+        <div className="absolute left-0 top-0 z-40 size-full">
+          <div className="mt-24 px-5 sm:px-10">
+            <h1 className="special-font hero-heading text-blue-100">
+              redefi<b>n</b>e
+            </h1>
+          </div>
+        </div>
       </div>
     </div>
   );
